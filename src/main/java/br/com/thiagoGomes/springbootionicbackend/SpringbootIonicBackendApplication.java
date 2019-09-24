cfc94132e3b9e525c81ef607f6ceab2015f3a7d1
Thiago Gomes <thiagogomes19@hotmail.com>
2019-09-23 22:37:07 -0300

    Criando Projeto

---
+++ src/main/java/br/com/thiagoGomes/springbootionicbackend/SpringbootIonicBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.thiagoGomes.springbootionicbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootIonicBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootIonicBackendApplication.class, args);
+	}
+
+}
