44c079535b6eaaddf92afb1b061a8b5d4b134b0a
Thiago Gomes <thiagogomes19@hotmail.com>
2019-09-28 23:28:35 -0300

    Testando a primeira classe de dominio - Categoria

--- src/main/java/br/com/thiagoGomes/resources/CategoriaResource.java
+++ src/main/java/br/com/thiagoGomes/resources/CategoriaResource.java
@@ -1,15 +1,27 @@
-package br.com.thiagoGomes.controllers;
+package br.com.thiagoGomes.resources;
 
+import br.com.thiagoGomes.domain.Categoria;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
+@RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
-    @RequestMapping(value="/circles", method=RequestMethod.GET)
-    public String listar() {
-        return "REST está funcionando!";
+    @GetMapping
+    public List<Categoria> listar() {
+
+        Categoria cat1 = new Categoria(1, "Informatica");
+        Categoria cat2 = new Categoria(2, "Escritório");
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(cat1);
+        lista.add(cat2);
+
+        return lista;
     }
 
 }
