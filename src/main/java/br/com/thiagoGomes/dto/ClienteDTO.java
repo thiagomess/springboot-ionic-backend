7da3699dd78c4deaf3157de8c05ae57f4b50c495
Thiago Gomes <thiagogomes19@hotmail.com>
2019-11-16 13:33:05 -0300

    Validação customizada: email não repetido na atualização de Cliente

--- src/main/java/br/com/thiagoGomes/dto/ClienteDTO.java
+++ src/main/java/br/com/thiagoGomes/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import br.com.thiagoGomes.domain.Cliente;
+import br.com.thiagoGomes.service.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 
 	private static final long serialVersionUID = 1L;
---
+++ src/main/java/br/com/thiagoGomes/service/validation/ClienteUpdateValidator.java
@@ -0,0 +1,55 @@
+package br.com.thiagoGomes.service.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
+
+import br.com.thiagoGomes.domain.Cliente;
+import br.com.thiagoGomes.dto.ClienteDTO;
+import br.com.thiagoGomes.repositories.ClienteRepository;
+import br.com.thiagoGomes.resources.exceptions.FieldMessage;
+
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private HttpServletRequest request;
+	
+	@Override
+	public void initialize(ClienteUpdate ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context) {
+		
+		@SuppressWarnings("unchecked")
+		Map<String, String> map = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		Integer uriId = Integer.parseInt(map.get("id"));
+		
+		List<FieldMessage> list = new ArrayList<>();
+// inclua os testes aqui, inserindo erros na lista
+		
+
+		//Valida se o email já existe no banco e devolve uma excecao personalizada
+		Cliente aux = clienteRepository.findByEmail(objDto.getEmail());
+		if (aux != null && !aux.getId().equals(uriId)) {
+			list.add(new FieldMessage("email", "Email já existente em outro cliente"));
+		}
+		
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
