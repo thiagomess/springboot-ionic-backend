608399ccdf8857b9566f38dd81bd1161f5decfce
Thiago Gomes <thiagogomes19@hotmail.com>
2019-11-20 15:51:31 -0300

    Subtotal e total do Pedido

--- src/main/java/br/com/thiagoGomes/domain/Pedido.java
+++ src/main/java/br/com/thiagoGomes/domain/Pedido.java
@@ -54,6 +54,10 @@ public class Pedido implements Serializable {
         this.cliente = cliente;
         this.enderecoDeEntrega = enderecoDeEntrega;
     }
+    
+    public double getValorTotal() {
+    	return itens.stream().mapToDouble(x -> x.getSubTotal()).sum();
+    }
 
     public Integer getId() {
         return id;
