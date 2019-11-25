fc91fcc4d859676006427a30fb95374e64b462c7
Thiago Gomes <thiagogomes19@hotmail.com>
2019-11-25 12:45:06 -0300

    Configuração inicial do Spring Security

---
+++ src/main/java/br/com/thiagoGomes/config/SecurityConfig.java
@@ -0,0 +1,65 @@
+package br.com.thiagoGomes.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	//Recupera o profile ativo
+	@Autowired
+	private Environment env;
+
+	private static final String[] PUBLIC_MATCHERS = {
+
+			"/h2-console/**" 
+			};
+	
+	private static final String[] PUBLIC_MATCHERS_GET = {
+
+			"/produtos/**", 
+			"/categorias/**" 
+			};
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+			http.headers().frameOptions().disable();
+			//Se tiver no perfil de Test, ira liberar acesso ao H2
+		}
+
+		http.cors() // ativa o bean CorsConfigurationSource
+				.and().csrf().disable(); // e Desativa o csrf pois a aplicação nao armazena sessao
+		http.authorizeRequests()
+			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll() //Permite apenas os metodos GET do Array
+			.antMatchers(PUBLIC_MATCHERS).permitAll()
+			.anyRequest().authenticated();
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);// Pra assegurar que nao ira
+																						// criar sessao
+
+		super.configure(http);
+	}
+
+	// Permite acesso aos endpoints por multiplas fontes com as configurações
+	// basicas
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+		return source;
+	}
+
+}
