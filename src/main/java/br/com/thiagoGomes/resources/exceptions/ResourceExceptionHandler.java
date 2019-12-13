7099dbe720a0b3c0841af9c640d06022bf5c8770
Thiago Gomes <thiagogomes19@hotmail.com>
2019-12-13 19:39:21 -0300

    Padronizando formato das exceções

--- src/main/java/br/com/thiagoGomes/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/br/com/thiagoGomes/resources/exceptions/ResourceExceptionHandler.java
@@ -1,6 +1,7 @@
 package br.com.thiagoGomes.resources.exceptions;
 
 import java.time.LocalDateTime;
+import java.util.Date;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -24,38 +25,34 @@ public class ResourceExceptionHandler {
 
 	@ExceptionHandler(ObjectNotFoundException.class)
 	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
-		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), LocalDateTime.now());
-
+		StandardError err = new StandardError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(),"Não encontrado", e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
 	}
 
 	@ExceptionHandler(DataIntegrityException.class)
 	public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request) {
-		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), LocalDateTime.now());
-
+		StandardError err = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(),"Integridade de dados", e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
 
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	public ResponseEntity<ValidationError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
-		ValidationError err = new ValidationError(HttpStatus.BAD_REQUEST.value(), "Erro de validação",
-				LocalDateTime.now());
+		ValidationError err = new ValidationError(System.currentTimeMillis(), HttpStatus.UNPROCESSABLE_ENTITY.value(),"Erro de validação", e.getMessage(), request.getRequestURI());
 
 		e.getBindingResult().getFieldErrors().forEach(x -> err.addError(x.getField(), x.getDefaultMessage()));
 
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+		return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(err);
 	}
 
 	@ExceptionHandler(AuthorizationException.class)
 	public ResponseEntity<StandardError> authorization(AuthorizationException e, HttpServletRequest request) {
-		StandardError err = new StandardError(HttpStatus.FORBIDDEN.value(), e.getMessage(), LocalDateTime.now());
-
+		StandardError err = new StandardError(System.currentTimeMillis(), HttpStatus.FORBIDDEN.value(),"Acesso negado", e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(HttpStatus.FORBIDDEN).body(err);
 	}
 
 	@ExceptionHandler(FileException.class)
 	public ResponseEntity<StandardError> file(FileException e, HttpServletRequest request) {
-		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), LocalDateTime.now());
+		StandardError err = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(),"Erro de arquivo", e.getMessage(), request.getRequestURI());
 
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
@@ -64,19 +61,20 @@ public class ResourceExceptionHandler {
 	public ResponseEntity<StandardError> amazonService(AmazonServiceException e, HttpServletRequest request) {
 
 		HttpStatus code = HttpStatus.valueOf(e.getErrorCode());
-		StandardError err = new StandardError(code.value(), e.getMessage(), LocalDateTime.now());
+		StandardError err = new StandardError(System.currentTimeMillis(), code.value(),"Erro Amazon Service", e.getMessage(), request.getRequestURI());
+
 		return ResponseEntity.status(code).body(err);
 	}
 
 	@ExceptionHandler(AmazonClientException.class)
 	public ResponseEntity<StandardError> authorization(AmazonClientException e, HttpServletRequest request) {
-		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), LocalDateTime.now());
+		StandardError err = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(),"Erro Amazon Client", e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
 	
 	@ExceptionHandler(AmazonS3Exception.class)
 	public ResponseEntity<StandardError> authorization(AmazonS3Exception e, HttpServletRequest request) {
-		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), LocalDateTime.now());
+		StandardError err = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(),"Erro S3", e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
 
--- src/main/java/br/com/thiagoGomes/resources/exceptions/StandardError.java
+++ src/main/java/br/com/thiagoGomes/resources/exceptions/StandardError.java
@@ -9,38 +9,71 @@ public class StandardError implements Serializable {
 
     private static final long serialVersionUID = -652941923666568157L;
 
+    private Long timeStamp;
     private Integer status;
-    private String msg;
-    @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
-    private LocalDateTime timeStamp;
+    private String error;
+    private String message;
+    private String path;
 
-    public StandardError(Integer status, String msg, LocalDateTime timeStamp) {
-        this.status = status;
-        this.msg = msg;
-        this.timeStamp = timeStamp;
-    }
+    
+    public StandardError(Long timeStamp, Integer status, String error, String message, String path) {
+		super();
+		this.timeStamp = timeStamp;
+		this.status = status;
+		this.error = error;
+		this.message = message;
+		this.path = path;
+	}
 
-    public Integer getStatus() {
-        return status;
-    }
 
-    public void setStatus(Integer status) {
-        this.status = status;
-    }
 
-    public String getMsg() {
-        return msg;
-    }
+	public Long getTimeStamp() {
+		return timeStamp;
+	}
 
-    public void setMsg(String msg) {
-        this.msg = msg;
-    }
 
-    public LocalDateTime getTimeStamp() {
-        return timeStamp;
-    }
+	public void setTimeStamp(Long timeStamp) {
+		this.timeStamp = timeStamp;
+	}
 
-    public void setTimeStamp(LocalDateTime timeStamp) {
-        this.timeStamp = timeStamp;
-    }
+
+	public Integer getStatus() {
+		return status;
+	}
+
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+
+	public String getError() {
+		return error;
+	}
+
+
+	public void setError(String error) {
+		this.error = error;
+	}
+
+
+	public String getMessage() {
+		return message;
+	}
+
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+
+	public String getPath() {
+		return path;
+	}
+
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+    
 }
